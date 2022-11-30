8f3cd84eb42eba06f9b6277cefc32a1a0c4700c6
Tuan Kiet <kiet.le0303@hcmut.edu.vn tuankiet0303@gmail.com>
2022-11-30 10:37:30 +0700

    Khai báo hàm dùng cho Morse

---
+++ morseEncoder.h
@@ -0,0 +1,48 @@
+#ifndef _MORSE_ENCODER
+#define _MORSE_ENCODER
+
+#include <Arduino.h>
+
+
+void dot();
+void dash();
+
+void a();
+void b();
+void c();
+void d();
+void e();
+void f();
+void g();
+void h();
+void k();
+void i();
+void j();
+void k();
+void l();
+void m();
+void n();
+void o();
+void p();
+void q();
+void r();
+void s();
+void v();
+void w();
+void x();
+void y();
+void z();
+
+void _1();
+void _2();
+void _3();
+void _4();
+void _5();
+void _6();
+void _7();
+void _8();
+void _9();
+
+
+
+#endif
