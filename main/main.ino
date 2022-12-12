09aefac22e62ab78672b57cc5a57ee1103488319
Tuan Kiet <kiet.le0303@hcmut.edu.vn tuankiet0303@gmail.com>
2022-12-12 09:19:09 +0700

    debug

--- main/main.ino
+++ main/main.ino
@@ -4,7 +4,7 @@
 void setup() 
 {
     keyboard.init(); //khởi tạo
-    pinMode(output_pin, OUPUT);
+    pinMode(output_pin, OUTPUT);
     digitalWrite(output_pin, LOW);
     lcd.begin(16,2); //dimension
     lcd.setCursor(0,0);
