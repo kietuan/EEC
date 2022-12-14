c3a666cd5153a3970eadda16e56b548194ecfe69
Tuan Kiet <kiet.le0303@hcmut.edu.vn tuankiet0303@gmail.com>
2022-12-14 19:09:24 +0700

    debug

--- main/main.ino
+++ main/main.ino
@@ -3,8 +3,8 @@
 
 void setup() 
 {
-    Serial.begin(9600); // opens serial port, sets data rate to 9600 bps
-    Serial.println("Getting started");
+    //Serial.begin(9600); // opens serial port, sets data rate to 9600 bps
+    //Serial.println("Getting started");
     //a();b();_c();
 
     keyboard.init(); //khởi tạo
