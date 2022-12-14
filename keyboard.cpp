8fddc968bbff0fef5e2e1e9ed8879fe9c5a4e74d
Tuan Kiet <kiet.le0303@hcmut.edu.vn tuankiet0303@gmail.com>
2022-12-14 10:43:34 +0700

    set 2

--- keyboard.cpp
+++ keyboard.cpp
@@ -9,91 +9,96 @@ uint8_t numOfChars = 0; //dùng để theo dõi số ký tự trên màn hình h
 void ps2keyboard::push(uint8_t scancode) //mỗi lần nhấn là phải đẩy lên màn hình, Nhận scancode, xuất ra mã ascii
 {
     char character = 0;
-    switch (scancode)
-    {
-        case 0x1c: //Enter //bắt đầu xả ra, nhưng không thể làm bằng interupt được
-            character = 0x0d; break;
-        case 0x0e: //backspace
-            keyboard.buffer.clear();
-            lcd.clear();
-            numOfChars = 0;
-            return;
+    static uint8_t prevScancode = 0;
 
-        case 0x0b:
-            character = '0'; break;
-        case 0x02:
-            character = '1'; break;
-        case 0x03:
-            character = '2'; break;
-        case 0x04:
-            character = '3'; break;
-        case 0x05:
-            character = '4'; break;
-        case 0x06:
-            character = '5'; break;
-        case 0x07:
-            character = '6'; break;
-        case 0x08:
-            character = '7'; break;
-        case 0x09:
-            character = '8';  break;
-        case 0x0a:
-            character = '9'; break;
-        
-        case 0x39:
-            character = ' '; break;
-        
-        case 0x1e:
-            character = 'a'; break;
-        case 0x30:
-            character = 'b'; break;
-        case 0x2e:
-            character = 'c'; break;
-        case 0x20:
-            character = 'd'; break;
-        case 0x12:
-            character = 'e'; break;
-        case 0x21:
-            character = 'f'; break;
-        case 0x22:
-            character = 'g'; break;
-        case 0x23:
-            character = 'h'; break;
-        case 0x17:
-            character = 'i'; break;
-        case 0x24:
-            character = 'j'; break;
-        case 0x25:
-            character = 'k'; break;
-        case 0x26:
-            character = 'l'; break;
-        case 0x32:
-            character = 'm'; break;
-        case 0x18:
-            character = 'o'; break;
-        case 0x19:
-            character = 'p'; break;
-        case 0x10:
-            character = 'q'; break;
-        case 0x13:
-            character = 'r'; break;
-        case 0x1f:
-            character = 's'; break;
-        case 0x14:
-            character = 't'; break;
-        case 0x16:
-            character = 'u'; break;
-        case 0x2f:
-            character = 'v'; break;
-        case 0x11:
-            character = 'w'; break;
-        case 0x2d:
-            character = 'x'; break;
-        case 0x15:
-            character = 'y'; break;
-        case 0x2c:
-            character = 'z'; break;
-    }
+    if (prevScancode != 0xf0)
+        switch (scancode)
+        {
+            case 0x5a: //Enter //bắt đầu xả ra, nhưng không thể làm bằng interupt được
+                character = 0x0d; break;
+            case 0x66: //backspace
+                keyboard.buffer.clear();
+                lcd.clear();
+                numOfChars = 0;
+                return;
+
+            case 0x45:
+                character = '0'; break;
+            case 0x16:
+                character = '1'; break;
+            case 0x1e:
+                character = '2'; break;
+            case 0x26:
+                character = '3'; break;
+            case 0x25:
+                character = '4'; break;
+            case 0x02e:
+                character = '5'; break;
+            case 0x36:
+                character = '6'; break;
+            case 0x3d:
+                character = '7'; break;
+            case 0x3e:
+                character = '8';  break;
+            case 0x46:
+                character = '9'; break;
+            
+            case 0x29:
+                character = ' '; break;
+            
+            case 0x1c:
+                character = 'a'; break;
+            case 0x32:
+                character = 'b'; break;
+            case 0x21:
+                character = 'c'; break;
+            case 0x23:
+                character = 'd'; break;
+            case 0x24:
+                character = 'e'; break;
+            case 0x2b:
+                character = 'f'; break;
+            case 0x34:
+                character = 'g'; break;
+            case 0x33:
+                character = 'h'; break;
+            case 0x43:
+                character = 'i'; break;
+            case 0x3b:
+                character = 'j'; break;
+            case 0x42:
+                character = 'k'; break;
+            case 0x4b:
+                character = 'l'; break;
+            case 0x3a:
+                character = 'm'; break;
+            case 0x44:
+                character = 'o'; break;
+            case 0x4d:
+                character = 'p'; break;
+            case 0x15:
+                character = 'q'; break;
+            case 0x2d:
+                character = 'r'; break;
+            case 0x1b:
+                character = 's'; break;
+            case 0x2c:
+                character = 't'; break;
+            case 0x3c:
+                character = 'u'; break;
+            case 0x2a:
+                character = 'v'; break;
+            case 0x1d:
+                character = 'w'; break;
+            case 0x22:
+                character = 'x'; break;
+            case 0x35:
+                character = 'y'; break;
+            case 0x1a:
+                character = 'z'; break;
+        }
+    
+    prevScancode = scancode;
 
     if (character) //bắt đầu viết lên
     {
@@ -112,43 +117,7 @@ void ps2keyboard::push(uint8_t scancode) //mỗi lần nhấn là phải đẩy 
         numOfChars++;
         if (numOfChars == 16) lcd.setCursor(0,1);
         
-
     }
-}
-
-
-void ps2interrupt()
-{
-    static uint8_t bitcount = 0; //số bit đã được đọc, tức là hiện tại đang có trong rawvalue
-    static uint8_t scancode = 0; 
-    static int16_t rawvalue = 0;
-
-    static unsigned long prevTime = 0;
-    unsigned long currentTime;
-    uint8_t bit;
-
-    bit = digitalRead(data_pin); //giá trị bit đã đọc được để đưa vào
-    currentTime = micros();
-    if (currentTime - prevTime > 500) //hết thời gian cho 1 chu kỳ, ở đây có thể xuất hiện bug, read https://www.networktechinc.com/ps2-prots.html https://www.sra.uni-hannover.de/Lehre/WS21/L_BST/doc/ps2.html
-    {
-        scancode = 0;
-        bitcount = 0;
-        rawvalue = 0;
-    }
-
-    rawvalue |= (bit << bitcount);
-    bitcount++;
-    if (bitcount == 11) //hết 1 gói bits
-    {
-        rawvalue &= 0b0000001111111100;
-        scancode = rawvalue >> 2;
-        //chuyển scancode này thành ascii rồi lưu vào buffer. nếu = 0 thì thôi đừng lưu, như vậy thì chỉ có 1 lần nhất là được ghi, lần nhả không ghi
-        keyboard.push(scancode);
-
-        rawvalue = 0;
-        scancode = 0;
-        bitcount = 0;
-    }    
 }
 
 void ps2keyboard::init()
